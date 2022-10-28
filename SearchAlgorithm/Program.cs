10350c131fe9594aadc8fc1c5007166fb1f709af
Prima06112002 <ramadhan06112002@gmail.com>
2022-10-28 16:34:01 +0700

    add method binary search

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -34,8 +34,49 @@ namespace SearchAlgorithm
             Console.WriteLine("-----------------");
             for (i = 0; i < n; i++)
             {
-
+                Console.WriteLine("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
             }
+        }
+        public void BinarySearch()
+        {
+            char ch;
+            do
+            {
+                //accept the number to be searched
+                Console.Write("\nEnter element want you to search : ");
+                int item = Convert.ToInt32(Console.ReadLine());
+
+                //apply binary seacrh
+                int lowerbound = 0;
+                int upperbound = n - 1;
+
+                //obtain the index of the element in the array
+                int mid = (lowerbound + upperbound) / 2;
+                int ctr = 1;
+
+                //loop to search for the element in the array
+                while ((item != arr[mid]) && (lowerbound <= upperbound))
+                {
+                    if (item > arr[mid])
+                        lowerbound = mid + 1;
+                    else
+                        upperbound = mid - 1;
+
+                    mid = (lowerbound + upperbound) / 2;
+                    ctr++;
+                }
+                if (item == arr[mid])
+                    Console.WriteLine("\n" + item.ToString() + "found at position" + (mid + 1).ToString());
+                else
+                    Console.WriteLine("\n" + item.ToString() + "not found in the array\n");
+                Console.WriteLine("\nNumber of comparison : " + ctr);
+
+                Console.Write("\nContinue search (y/n):");
+                ch = char.Parse(Console.ReadLine());
+
+            } while ((ch == 'y') || (ch == 'Y'));
         }
     }
 }
