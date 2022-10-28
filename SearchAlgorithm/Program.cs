af70d3eb5cdc3b912b0b74ed36686332aa8cb007
Prima06112002 <ramadhan06112002@gmail.com>
2022-10-28 16:07:30 +0700

    add method input

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -8,8 +8,25 @@ namespace SearchAlgorithm
 {
     internal class Program
     {
-        static void Main(string[] args)
+        //Array to be searched
+        int[] arr = new int[20];
+        //Number of element in the array
+        int n;
+        //get the number of element to store in the array
+        int i;
+
+        public void input()
         {
+            while (true)
+            {
+                Console.Write("Enter the number of element in the array");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine("\n Array should have minimum 1 and maximum 20 element.\n");
+            }
         }
     }
 }
